93970277256ba6616bebe7b9301bc4cee7ca76b9
RK5456 <rk545@kent.ac.uk>
2024-12-04 05:42:15 +0000

    code4.py

--- code4.py
+++ code4.py
@@ -1,5 +1,5 @@
 # here I will use a modulo operation and case for BMW HP recognition, if the remainder is 0 its the best and below it becomes more fuel economy or not recognised
-
+# note to self: this uses python 3.10 and higher, code was simulated on a higher source.
 
 def bmwhp(hp):
     # match is used to find horsepower of cars and its levels
